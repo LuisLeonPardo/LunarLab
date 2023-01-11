b02dab38836d054595d84395d82fee3a1a126684
FacundoMartinez14 <facundo.eet2@gmail.com>
2023-01-11 18:58:38 -0300

    tareas finalizadas

--- client/src/components/home/BottomHome/index.jsx
+++ client/src/components/home/BottomHome/index.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Footer from '../Footer/Footer'
-// import NavBarBottomHome from '../NavBarBottomHome'
 import SectionCards from '../SectionCards'
 import styles from './index.module.css'
 function BottomHome() {
   return (
     <div className={styles.bottomHome}>
-    {/* <NavBarBottomHome /> */}
     <SectionCards />
-    {/* <Footer /> */}
     </div>
   )
 }
