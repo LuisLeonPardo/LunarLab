652c6dcd04b77d4e657e45aa3ca89208e365f487
FacundoMartinez14 <facundo.eet2@gmail.com>
2023-01-11 15:08:00 -0300

    Terminada las cards y el responsive del mismo

--- client/src/components/home/NavBarBottomHome/index.jsx
+++ client/src/components/home/NavBarBottomHome/index.jsx
@@ -14,37 +14,48 @@ function NavBarBottomHome() {
 		setUpcomming(false);
 	};
 	return (
-		<div className={styles.navBarHomeBottom}>
-			<div>
-				<button
-					onClick={handleUpcomming}
-					className={`${upcomming ? styles.active : styles.notActive} ${styles.upcomming}`}
-				>
-					Upcoming
-				</button>
-				<button
-					onClick={handleEnded}
-					className={`${ended ? styles.active : styles.notActive} ${styles.ended}`}
-				>
-					Ended
-				</button>
-			</div>
-			<div className={styles.inputWrapper}>
-				<input type="search" className={styles.input} placeholder="Search pools" />
-
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					className={styles.inputIcon}
-					viewBox="0 0 20 20"
-					fill="currentColor"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-						clip-rule="evenodd"
+		<div className={styles.navBarWrap}>
+			<div className={styles.navBarHomeBottom}>
+				<div>
+					<button
+						onClick={handleUpcomming}
+						className={`${upcomming ? styles.active : styles.notActive} ${
+							styles.upcomming
+						}`}
+					>
+						Upcoming
+					</button>
+					<button
+						onClick={handleEnded}
+						className={`${ended ? styles.active : styles.notActive} ${
+							styles.ended
+						}`}
+					>
+						Ended
+					</button>
+				</div>
+				<div className={styles.inputWrapper}>
+					<input
+						type="search"
+						className={styles.input}
+						placeholder="Search pools"
 					/>
-				</svg>
+
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className={styles.inputIcon}
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							fillRule="evenodd"
+							d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</div>
 			</div>
+			<h1>Upcomming IDOs</h1>
 		</div>
 	);
 }
