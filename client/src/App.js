bf525c7252071fbb6aa9dfb983aef1fda314cfdd
Jhony A Saenz <jash0310@gmail.com>
2023-01-11 09:55:04 -0500

    Creando NavBar - finalizando navbar v3

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home/Home';
+import Home from './components/home/Home';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
