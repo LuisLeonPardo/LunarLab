6b533c0e712b537d116683f5494fb2e0c005ea7f
Willians <williansaaid@gmail.com>
2023-01-27 10:55:01 -0500

    Contact Component Done (Responsive Missing)

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -15,21 +15,41 @@ const Contact = () => {
                     <p className={styles.whiteText}>
                         LunarLab is willing to receive information about your project and carry out the fundraising process through our launch platform. We have an online application process that ensures that your project meets the requirements to be submitted for community approval. If you are interested, please complete the application form and await further instructions to start the process.
                     </p>
-                    <a className={styles.whiteButton}>Apply to Launch</a>
+                    <a
+                        className={styles.whiteButton}
+                        href="#"
+                    >
+                        Apply to Launch
+                    </a>
                 </div>
-                <img src={flying} alt='mascot flying' className={styles.imageHalf}/>
+                <img
+                    src={flying}
+                    alt='mascot flying'
+                    className={styles.firstImageHalf}
+                    draggable={false}
+                />
             </section>
             <section className={styles.secondSection}>
-                <div className={styles.textHalf}>
+                <div className={styles.secondTextHalf}>
                     <h3 className={styles.whiteMidTitle}>
                         Are you a top-tier project building the future?
                     </h3>
                     <p className={styles.whiteThinText}>
                         Get in touch with us today to discuss launching with Lunar Lab collaborative incubation and funding opportunities, and access to the Lunar Lab
                     </p>
-                    <a className={styles.whiteButton}>Schedule a Meeting</a>
+                    <a
+                        className={styles.whiteButton}
+                        href="#"
+                    >
+                        Schedule a Meeting
+                    </a>
                 </div>
-                <img src={investing} alt='mascot investing' className={styles.imageHalf}/>
+                <img
+                    src={investing}
+                    alt='mascot investing'
+                    className={styles.secondImageHalf}
+                    draggable={false}
+                />
             </section>
             <section className={styles.thirdSection}>
                 <h4 className={styles.whiteSmallTitle}>
@@ -38,7 +58,12 @@ const Contact = () => {
                 <p className={styles.grayText}>
                     Join our VC team, within Keter Capital
                 </p>
-                <a className={styles.blueButton}>Apply</a>
+                <a
+                    className={styles.blueButton}
+                    href="#"
+                >
+                    Apply
+                </a>
             </section>
             <Footer/>
         </div>
