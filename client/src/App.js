0a40e9d625b81b38d8cca3229ef0b57dd5875b7d
Agustin Panizza <agustin_panizza@hotmail.com>
2023-02-01 14:57:55 -0300

    wallet connect

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,18 @@
 // import './App.css';
-import Home from './components/home/Home';
-import { Route, Routes } from 'react-router-dom';
-import Footer from './components/home/Footer';
-import Modal from './components/home/Modal';
+import Home from "./components/home/Home";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/home/Footer";
+import Modal from "./components/home/Modal";
 
 function App() {
-<<<<<<< HEAD
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+      <Routes>
+        <Route path="/home" element={[<Home />, <Footer />]} />
+        {/* <Route path="/modal" element={<Modal />} /> */}
+      </Routes>
     </div>
   );
-=======
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/home" element={[<Home />, <Footer />]} />
-				{/* <Route path="/modal" element={<Modal />} /> */}
-			</Routes>
-		</div>
-	);
->>>>>>> 3249a83bf191bd523a697690bc9b93e0f1ce5be5
 }
 
 export default App;
--- client/src/components/home/Navbar/index.jsx
+++ client/src/components/home/Navbar/index.jsx
@@ -1,79 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import './index.css'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./index.css";
 
-import LunarLab from '../../Assets/Group 27848.svg'
+import LunarLab from "../../Assets/Group 27848.svg";
+import CustomConnectButtom from "../../WalletConnect/CustomConnectButtom";
+import style from "../../WalletConnect/CustomConnect.module.scss";
 
 function Navbar() {
-    const location = useLocation();
-    const [homeLinkClass, setHomeLinkClass] = useState('');
-    const [isVisible, setIsVisible] = useState(false);
+  const location = useLocation();
+  const [homeLinkClass, setHomeLinkClass] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
 
-    function handleResize() {
-        if (window.innerWidth <= 575) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
+  function handleResize() {
+    if (window.innerWidth <= 575) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  }
+
+  useEffect(() => {
+    if (location.pathname === "/home") {
+      setHomeLinkClass("active-link-home");
+    } else {
+      setHomeLinkClass("active-link-home-no");
     }
 
-    useEffect(() => {
-        if (location.pathname === '/home') {
-            setHomeLinkClass('active-link-home');
-        } else {
-            setHomeLinkClass('active-link-home-no');
-        }
+    // if (location.pathname === '/contacts') {
+    //     setHomeLinkClass('active-link-contacts');
+    // } else {
+    //     setHomeLinkClass('active-link-contacts-no');
+    // }
 
-        // if (location.pathname === '/contacts') {
-        //     setHomeLinkClass('active-link-contacts');
-        // } else {
-        //     setHomeLinkClass('active-link-contacts-no');
-        // }
+    window.addEventListener("resize", handleResize);
 
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [location]);
 
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-
-    }, [location]);
-
-
-    return (
-        <div className="contenedor-navBar">
-
-            <div className='container'>
-                {/* <div className='pixeles'>Pixeles</div> */}
-                <div className='navBar'>
-                    <div className='container-img-btn'>
-                        <div className='navBar-iconoLunarLab'>
-                            <img src={LunarLab} alt="LunarLab" />
-                        </div>
-                        <div>
-                            <button className={`menu-button ${isVisible ? 'visible' : 'hidden'}`}>
-                                <span>Menu</span>
-                            </button>
-                        </div>
-
-                    </div>
-
-                    <div className='navBar-iconosNavBar'>
-                        <div className='navBarLinks'>
-                            <Link to="/home" className={`nav-link ${homeLinkClass}`}>Home</Link>
-                            <Link id='navBar-btn-whitepaper' to="" className="nav-link">Whitepaper</Link>
-                            <Link id="navBar-btn-contacts" to="/contacts" className="nav-link">Contacts</Link>
-                            <Link id='navBar-btn-faq' to="" className="nav-link">FAQ</Link>
-
-                        </div>
-                        <button className="navBar-btn-connect">Connect Wallet</button>
-                    </div>
-                </div>
+  return (
+    <div className="contenedor-navBar">
+      <div className="container">
+        {/* <div className='pixeles'>Pixeles</div> */}
+        <div className="navBar">
+          <div className="container-img-btn">
+            <div className="navBar-iconoLunarLab">
+              <img src={LunarLab} alt="LunarLab" />
             </div>
+            <div>
+              <button
+                className={`menu-button ${isVisible ? "visible" : "hidden"}`}
+              >
+                <span>Menu</span>
+              </button>
+            </div>
+          </div>
 
+          <div className="navBar-iconosNavBar">
+            <div className="navBarLinks">
+              <Link to="/home" className={`nav-link ${homeLinkClass}`}>
+                Home
+              </Link>
+              <Link id="navBar-btn-whitepaper" to="" className="nav-link">
+                Whitepaper
+              </Link>
+              <Link
+                id="navBar-btn-contacts"
+                to="/contacts"
+                className="nav-link"
+              >
+                Contacts
+              </Link>
+              <Link id="navBar-btn-faq" to="" className="nav-link">
+                FAQ
+              </Link>
+            </div>
+            <CustomConnectButtom container={style.OpenContainer} />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
