b963f2080f16c3b37fd52ed5611b52830fd4ddb5
Camilo <churtadoh92@gmail.com>
2023-02-01 16:18:45 -0500

    Finished general layout

---
+++ client/src/components/Profile/index.jsx
@@ -0,0 +1,88 @@
+import styles from "./index.module.css"
+import question from "../Assets/question.svg"
+import FCard from "./FCard"
+import info from "./info.json"
+import { useState } from "react"
+import { useEffect } from "react"
+
+export default function Profile() {
+    const [forsage, setForsage] = useState()
+
+    useEffect(()=>{
+        setForsage(info.data)
+    })
+
+
+    return(
+        <div className={styles.generalP}>
+            <div className={styles.contentP}>
+               <div className={styles.headerP}>
+                    <div className={styles.profileP}>
+
+                    </div>
+                    <div className={styles.linkP}>
+
+                    </div>
+               </div>
+               <div className={styles.infoP}>
+                    <div className={styles.rowP}>
+                        <div className={styles.card1}>
+                            Partners
+                        </div>
+                        <div className={styles.card1}>
+                            Team
+                        </div>
+                        <div className={styles.card1}>
+                            Ratio
+                        </div>
+                    </div>
+                    <div className={styles.profitsP}>
+                        Profits
+                    </div>
+               </div>
+               <div className={styles.forsageP}>
+                    <div className={styles.fheader}>
+                        <h1 className={styles.title1}>Forsage Programs</h1>
+                        <div className={styles.infoRow}>
+                            <img src={question}/>
+                            <p className={styles.textInfo}>Info</p>
+                        </div>
+                    </div>
+                    <div className={styles.gridP}>
+                        {forsage && forsage.map((el,index) => 
+                        <FCard
+                        key={index}
+                        />
+                        )}
+                    </div>
+               </div>
+               <div className={styles.bodyP}>
+                    <div className={styles.activityH}>
+                        <h1 className={styles.title1}>Platform recent activity</h1>
+                        <img src={question}/>
+                    </div>
+                    <div className={styles.activityC}>
+                        <div className={styles.container1}>
+
+                        </div>
+                        <div className={styles.column}>
+                            <div className={styles.container2}>
+
+                            </div>
+                            <div className={styles.container2}>
+                                
+                            </div>
+                            <div className={styles.container3}>
+                                
+                            </div>
+                        </div>
+                    </div>
+               </div>
+               <div className={styles.disclaimerP}>
+                    <h6 className={styles.textDisclaimer}>2020 All Rights Reserved</h6>
+                    <h6 className={styles.textDisclaimer}>Disclaimer</h6>
+               </div>
+            </div>
+        </div>
+    )
+};
