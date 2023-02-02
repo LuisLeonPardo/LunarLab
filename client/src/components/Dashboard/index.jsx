ecd9395a6d19fea6a1616988c6a6a4f0be17542b
FacundoMartinez14 <facundo.eet2@gmail.com>
2023-02-02 19:34:15 -0300

    dashboard v2

--- client/src/components/Dashboard/index.jsx
+++ client/src/components/Dashboard/index.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import AccountPreview from './AccountPreview'
-import AsideCards from './AsideCards'
-import Header from './Header'
-import styles from './index.module.css'
-import SectionActivity from './SectionActivity'
+import React from 'react';
+import AccountPreview from './AccountPreview';
+import AsideCards from './AsideCards';
+import Header from './Header';
+import styles from './index.module.css';
+import SectionActivity from './SectionActivity';
 function Dashboard() {
-  return (
-    <div>
-      <Header />
-      <AccountPreview />
-      {/* <SectionActivity />
-      <AsideCards /> */}
-    </div>
-  )
+	return (
+		<div className={`${styles.dashboard}`}>
+			<Header />
+			<AccountPreview />
+			<section className={`${styles.dashboard__section}`}>
+				{/* <SectionActivity /> */}
+				<AsideCards />
+			</section>
+		</div>
+	);
 }
 
-export default Dashboard
+export default Dashboard;
