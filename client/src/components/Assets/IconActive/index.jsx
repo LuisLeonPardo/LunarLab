0996ffe16eb435ddc2a072e900e8cba5d4be4566
FacundoMartinez14 <facundo.eet2@gmail.com>
2023-02-03 22:03:46 -0300

    dashboard log out

---
+++ client/src/components/Assets/IconActive/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import sty from './index.module.css'
+function IconActive() {
+  return (
+    <div className={`${sty.first}`}>
+        <div className={`${sty.second}`}>
+            <div className={`${sty.third}`}>
+                <div className={`${sty.fourth}`}>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default IconActive
--- client/src/components/Dashboard/SectionActivity/index.jsx
+++ client/src/components/Dashboard/SectionActivity/index.jsx
@@ -1,18 +1,116 @@
 import React from 'react';
 import sty from './index.module.css';
 import RowActivity from './RowActivity';
-import {AiFillEye} from 'react-icons/ai'
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import { AiFillEye } from 'react-icons/ai';
+const arr = [
+	{
+		id: '196358',
+		contract: 'xGold',
+		time: '1 hour',
+		action: 'buy',
+		value: '38',
+	},
+	{
+		id: '196370',
+		contract: 'xXx',
+		time: '1 hour',
+		action: 'activates',
+		value: '38',
+	},
+	{
+		id: '190158',
+		contract: 'xGold',
+		time: '1 hour',
+		action: 'add',
+		value: '38',
+	},
+	{
+		id: '206358',
+		contract: 'x4',
+		time: '1 hour',
+		action: 'buy',
+		value: '85',
+	},
+	{
+		id: '196558',
+		contract: 'xXx',
+		time: '1 hour',
+		action: 'bonus',
+		value: '38',
+	},
+	{
+		id: '196387',
+		contract: 'xGold',
+		time: '1 hour',
+		action: 'bonus',
+		value: '100',
+	},
+	{
+		id: '196359',
+		contract: 'x4',
+		time: '1 hour',
+		action: 'buy',
+		value: '55',
+	},
+	{
+		id: '196360',
+		contract: 'xGold',
+		time: '1 hour',
+		action: 'activates',
+		value: '71',
+	},
+	{
+		id: '196331',
+		contract: 'xGold',
+		time: '1 hour',
+		action: 'add',
+		value: '50',
+	},
+	{
+		id: '196361',
+		contract: 'x3',
+		time: '1 hour',
+		action: 'buy',
+		value: '40',
+	},
+	{
+		id: '758921',
+		contract: 'x3',
+		time: '1 hour',
+		action: 'bonus',
+		value: '120',
+	},
+	{
+		id: '789456',
+		contract: 'x4',
+		time: '1 hour',
+		action: 'buy',
+		value: '82',
+	},
+];
 function SectionActivity() {
 	return (
 		<div className={`${sty.activity}`}>
-			{arr.map((e, index) => (
-				<>
-					<RowActivity />
-          {index === arr.length - 1 ? null : <hr className={`${sty.activity__hr}`} />}
-				</>
-			))}
-      <button className={`button ${sty.activity__button}`}><AiFillEye />See more</button>
+			<div className={`${sty.activity__div}`}>
+				{arr.map((e, index) => (
+					<>
+						<RowActivity
+							id={e.id}
+							contract={e.contract}
+							time={e.time}
+							action={e.action}
+							value={e.value}
+						/>
+						{index === arr.length - 1 ? null : (
+							<hr className={`${sty.activity__hr}`} />
+						)}
+					</>
+				))}
+			</div>
+			<button className={`button ${sty.activity__button}`}>
+				<AiFillEye />
+				See more
+			</button>
 		</div>
 	);
 }
