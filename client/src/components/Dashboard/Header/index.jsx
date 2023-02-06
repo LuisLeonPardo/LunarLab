e5a9ef9b20e8a7cc7688b070f5d8deb2f8ecb73f
Agustin Panizza <agustin_panizza@hotmail.com>
2023-02-06 19:10:34 -0300

    navbar dashboard mobile, register responsive

--- client/src/components/Dashboard/Header/index.jsx
+++ client/src/components/Dashboard/Header/index.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import styles from './index.module.css'
-import img from '../../Assets/headerimg.svg'
+import React from "react";
+import styles from "./index.module.css";
+import img from "../../Assets/headerimg.svg";
+import CustomConnectButtom from "../../WalletConnect/CustomConnectButtom";
+import style from "../../WalletConnect/CustomConnect.module.scss";
+
 function Header() {
-	return (
-		<header className={`${styles.header}`}>
-			<h1 className={`${styles.header__h1}`}>LunarLab main page</h1>
-      <p className={`${styles.header__p}`} >Connect wallet to Register or Login</p>
-			<button className={`button ${styles.header__buttonConnect}`}>Connect now</button>
-			<button className={`button ${styles.header__buttonHelp}`}>Help me</button>
+  return (
+    <header className={`${styles.header}`}>
+      <h1 className={`${styles.header__h1}`}>LunarLab main page</h1>
+      <p className={`${styles.header__p}`}>
+        Connect wallet to Register or Login
+      </p>
+      <div className={`${styles.header__buttonConnect}`}>
+        <CustomConnectButtom container={style.OpenContainer} />
+      </div>
+      <button className={`button ${styles.header__buttonHelp}`}>Help me</button>
       <img src={img} alt="autopilot on" className={`${styles.header__img}`} />
-		</header>
-	);
+    </header>
+  );
 }
 
 export default Header;
