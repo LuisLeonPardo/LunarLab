e7ac9b6833b7bbe1c6a1257150bd302506f3b5ba
FacundoMartinez14 <facundo.eet2@gmail.com>
2023-02-06 18:41:52 -0300

    dashboard logged in

--- client/src/components/WalletConnect/CustomConnectButtom.jsx
+++ client/src/components/WalletConnect/CustomConnectButtom.jsx
@@ -1,80 +1,103 @@
-import React from "react";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import style from "./CustomConnect.module.scss";
+import React from 'react';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import style from './CustomConnect.module.scss';
+import {useNavigate} from 'react-router-dom'
+function CustomConnectButtom({ container, grid, logged }) {
+  const navigate = useNavigate()
+	return (
+		<div className={`${grid ? style.grid : style.button}`}>
+			<ConnectButton.Custom>
+				{({
+					account,
+					chain,
+					openAccountModal,
+					openChainModal,
+					openConnectModal,
+					authenticationStatus,
+					mounted,
+				}) => {
+					// Note: If your app doesn't use authentication, you
+					// can remove all 'authenticationStatus' checks
+					const ready = mounted && authenticationStatus !== 'loading';
+					const connected =
+						ready &&
+						account &&
+						chain &&
+						(!authenticationStatus || authenticationStatus === 'authenticated');
+					return (
+						<div
+							{...(!ready && {
+								'aria-hidden': true,
+								style: {
+									opacity: 0,
+									pointerEvents: 'none',
+									userSelect: 'none',
+								},
+							})}
+						>
+							{(() => {
+								if (!connected) {
+									return (
+										<div
+											className={`${
+												grid ? style.dashboardButton : style.button
+											}`}
+										>
+											{logged ? (
+												<button
+													onClick={() => navigate('/register')}
+													type="button"
+													className={`button ${
+														container === style.OpenContainer
+															? grid
+																? style.walletButtonOpenDashboard
+																: style.walletButtonOpen
+															: style.walletButton
+													}`}
+												>
+													Join Forsage
+												</button>
+											) : (
+												<button
+													onClick={openConnectModal}
+													type="button"
+													className={`button ${
+														container === style.OpenContainer
+															? grid
+																? style.walletButtonOpenDashboard
+																: style.walletButtonOpen
+															: style.walletButton
+													}`}
+												>
+													Connect Wallet
+												</button>
+											)}
+										</div>
+									);
+								}
 
-function CustomConnectButtom({ container }) {
-  return (
-    <div className={style.button}>
-      <ConnectButton.Custom>
-        {({
-          account,
-          chain,
-          openAccountModal,
-          openChainModal,
-          openConnectModal,
-          authenticationStatus,
-          mounted,
-        }) => {
-          // Note: If your app doesn't use authentication, you
-          // can remove all 'authenticationStatus' checks
-          const ready = mounted && authenticationStatus !== "loading";
-          const connected =
-            ready &&
-            account &&
-            chain &&
-            (!authenticationStatus || authenticationStatus === "authenticated");
-          return (
-            <div
-              {...(!ready && {
-                "aria-hidden": true,
-                style: {
-                  opacity: 0,
-                  pointerEvents: "none",
-                  userSelect: "none",
-                },
-              })}
-            >
-              {(() => {
-                if (!connected) {
-                  return (
-                    <div className={style.button}>
-                      <button
-                        onClick={openConnectModal}
-                        type="button"
-                        className={
-                          container === style.OpenContainer
-                            ? style.walletButtonOpen
-                            : style.walletButton
-                        }
-                      >
-                        Connect Wallet
-                      </button>
-                    </div>
-                  );
-                }
+								if (chain.unsupported) {
+									return (
+										<button onClick={openChainModal} type="button">
+											Wrong network
+										</button>
+									);
+								}
 
-                if (chain.unsupported) {
-                  return (
-                    <button onClick={openChainModal} type="button">
-                      Wrong network
-                    </button>
-                  );
-                }
-
-                return (
-                  <div
-                    className={
-                      container === style.OpenContainer
-                        ? style.ConnectedModal
-                        : style.ConnectedModalClosed
-                    }
-                  >
-                    <button
-                      onClick={openChainModal}
-                      type="button"
-                      className={style.chainModal}
-                    >
-                      {/* {chain.hasIcon && (
+								return (
+									<div
+										className={
+											container === style.OpenContainer
+												? style.ConnectedModal
+												: style.ConnectedModalClosed
+										}
+									>
+										<button
+											onClick={openChainModal}
+											type="button"
+											className={style.chainModal}
+										>
+											{/* {chain.hasIcon && (
                           <div
                             style={{
                               background: chain.iconBackground,
@@ -94,28 +117,28 @@ function CustomConnectButtom({ container }) {
                             )}
                           </div>
                         )} */}
-                      {chain.name}
-                    </button>
+											{chain.name}
+										</button>
 
-                    <button
-                      onClick={openAccountModal}
-                      type="button"
-                      className={style.balanceModal}
-                    >
-                      {account.displayName}
-                      {account.displayBalance
-                        ? ` (${account.displayBalance})`
-                        : ""}
-                    </button>
-                  </div>
-                );
-              })()}
-            </div>
-          );
-        }}
-      </ConnectButton.Custom>
-    </div>
-  );
+										<button
+											onClick={openAccountModal}
+											type="button"
+											className={style.balanceModal}
+										>
+											{account.displayName}
+											{account.displayBalance
+												? ` (${account.displayBalance})`
+												: ''}
+										</button>
+									</div>
+								);
+							})()}
+						</div>
+					);
+				}}
+			</ConnectButton.Custom>
+		</div>
+	);
 }
 
 export default CustomConnectButtom;
