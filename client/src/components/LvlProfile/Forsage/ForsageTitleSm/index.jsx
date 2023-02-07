34ed8175a6a9189bb9423ea764dad939f72b9004
Jhony Saenz <jash0310@gmail.com>
2023-02-07 15:02:45 -0500

    Añadiendo cards y otras detalles

---
+++ client/src/components/LvlProfile/Forsage/ForsageTitleSm/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+function ForsageTitleSm() {
+    return (
+        <div className='forsage-title-sm'>
+            <span id='forsage-tilte-id'>ID 843032 / &nbsp;</span>
+            <span id='forsage-tilte-por-tres'> &nbsp;Forsage x3 &nbsp;</span>
+            <span id='forsage-tilte-levels'>(2 out of 12 levels)</span>
+        </div>
+    )
+}
+
+export default ForsageTitleSm
