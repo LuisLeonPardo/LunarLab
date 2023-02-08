07d52ec6733381471c57556538a6e6efa105e145
Agustin Panizza <agustin_panizza@hotmail.com>
2023-02-08 17:05:58 -0300

    Dashboard real info , disconnect buttom and register

--- client/src/components/Dashboard/Header/index.jsx
+++ client/src/components/Dashboard/Header/index.jsx
@@ -1,55 +1,88 @@
-import React, { useState } from 'react';
-import styles from './index.module.css';
-import img from '../../Assets/headerimg.svg';
-import CustomConnectButtom from '../../WalletConnect/CustomConnectButtom';
-import style from '../../WalletConnect/CustomConnect.module.scss';
-import { SiBinance } from 'react-icons/si';
-import busd from '../../Assets/BUSD.png';
-import { RiBook3Line } from 'react-icons/ri';
-import { HiOutlineLogout } from 'react-icons/hi';
-import { RxDividerVertical } from 'react-icons/rx';
+import React, { useState } from "react";
+import styles from "./index.module.css";
+import img from "../../Assets/headerimg.svg";
+import CustomConnectButtom from "../../WalletConnect/CustomConnectButtom";
+import style from "../../WalletConnect/CustomConnect.module.scss";
+import { SiBinance } from "react-icons/si";
+import busd from "../../Assets/BUSD.png";
+import { RiBook3Line } from "react-icons/ri";
+import { HiOutlineLogout } from "react-icons/hi";
+import { RxDividerVertical } from "react-icons/rx";
+import { useAccount, useNetwork, useBalance } from "wagmi";
+import { useAccountModal } from "@rainbow-me/rainbowkit";
+import { NavLink } from "react-router-dom";
 function Header() {
-	const [logged, setlogged] = useState(true);
-	return (
-		<header className={`${styles.header}`}>
-			{logged ? (
-				<div className={styles.logged}>
-					<SiBinance className={`${styles.currencyIcon} ${styles.binance}`} />
-					<span className={`${styles.smart}`}>Smart Chain</span>
-					<img
-						src={busd}
-						alt="BUSD"
-						className={`${styles.currencyIcon} ${styles.busd}`}
-					/>
-					<span>0.00 BUSD</span>
-					<RxDividerVertical className={styles.vr} />
-					<SiBinance
-						className={`${styles.currencyIcon} ${styles.binanceGold}`}
-					/>
-					<span className={`${styles.bnb}`}>0.000 BNB</span>
-					<RxDividerVertical className={styles.vr} />
-					<RiBook3Line />
-					<span className={`${styles.contract}`}>0xac...a4</span>
-					<button className={`${styles.logout}`} onClick={() => setlogged(false)}>
-						<HiOutlineLogout className={styles.logoutIcon} />
-					</button>
-				</div>
-			) : null}
-			<h1 className={`${styles.header__h1}`}>
-				{logged ? 'Register in LonarLab' : 'LunarLab main page'}
-			</h1>
-			<p className={`${styles.header__p}`}>
-				Connect wallet to Register or Login
-			</p>
-			<CustomConnectButtom
-				container={style.OpenContainer}
-				grid={true}
-				logged={logged}
-			/>
-			<button className={`button ${styles.header__buttonHelp}`}>Help me</button>
-			<img src={img} alt="autopilot on" className={`${styles.header__img}`} />
-		</header>
-	);
+  const { address, isConnected } = useAccount();
+  const { chain } = useNetwork();
+
+  const { openAccountModal } = useAccountModal();
+
+  const { data } = useBalance({
+    address: address,
+  });
+
+  const balanceBUSD = useBalance({
+    address: address,
+    token: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+  });
+
+  console.log(chain);
+
+  const cutAddres =
+    address?.slice(0, 4) +
+    "..." +
+    address?.slice(address?.length - 4, address?.length);
+
+  return (
+    <header className={`${styles.header}`}>
+      {isConnected ? (
+        <div className={styles.logged}>
+          <SiBinance className={`${styles.currencyIcon} ${styles.binance}`} />
+          <span className={`${styles.smart}`}>{chain.name}</span>
+          <img
+            src={busd}
+            alt="BUSD"
+            className={`${styles.currencyIcon} ${styles.busd}`}
+          />
+          <span>
+            {balanceBUSD.data.formatted + " " + balanceBUSD.data.symbol}
+          </span>
+          <RxDividerVertical className={styles.vr} />
+          <SiBinance
+            className={`${styles.currencyIcon} ${styles.binanceGold}`}
+          />
+          <span className={`${styles.bnb}`}>
+            {data.formatted + " " + data.symbol}
+          </span>
+          <RxDividerVertical className={styles.vr} />
+          <RiBook3Line />
+          <span className={`${styles.contract}`}>{cutAddres}</span>
+          <button
+            className={`${styles.logout}`}
+            onClick={() => openAccountModal()}
+          >
+            <HiOutlineLogout className={styles.logoutIcon} />
+          </button>
+        </div>
+      ) : null}
+      <h1 className={`${styles.header__h1}`}>
+        {isConnected ? "Register in LunarLab" : "LunarLab main page"}
+      </h1>
+      <p className={`${styles.header__p}`}>
+        Connect wallet to Register or Login
+      </p>
+      {isConnected ? (
+        <NavLink to={"/register"} className={`${styles.header__buttonConnect}`}>
+          {" "}
+          Join Forsage{" "}
+        </NavLink>
+      ) : (
+        <CustomConnectButtom container={style.OpenContainer} grid={true} />
+      )}
+      <button className={`button ${styles.header__buttonHelp}`}>Help me</button>
+      <img src={img} alt="autopilot on" className={`${styles.header__img}`} />
+    </header>
+  );
 }
 
 export default Header;
